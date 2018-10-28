90d3491a8d813bdf2880cbb57645726e32e50612
andreyloktev <loktev.andrei@gmail.com>
2018-10-28 12:28:13 +0300

    Added build script and .gitignore

--- build/build.cmd
+++ build/build.cmd
@@ -1,5 +1,3 @@
 @echo off
 
-del /Q /S .\*
-rmdir /Q *
-cmake .. -D-DCMAKE_TOOLCHAIN_FILE=%BUILDTLS%\vcpkg\scripts\buildsystems\vcpkg.cmake
+cmake .. -DCMAKE_TOOLCHAIN_FILE=%BUILDTLS%\vcpkg\scripts\buildsystems\vcpkg.cmake
